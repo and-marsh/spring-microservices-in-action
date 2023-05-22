e9ebf5975955858433aed792431618a978d078e7
Andrei Marshalov <andrei.marshalov@emlid.com>
2023-05-22 21:47:15 +0200

    repo: add `license-service` service skeleton

---
+++ licensing-service/src/main/kotlin/com/optimagrowth/license/LicensingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.optimagrowth.license
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LicensingServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LicensingServiceApplication>(*args)
+}
