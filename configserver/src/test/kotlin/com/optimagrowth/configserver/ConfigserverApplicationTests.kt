4b6dcb7bdf46345dfdbbc79eb06fd28a1e43c719
Andrei Marshalov <andrei.marshalov@emlid.com>
2023-07-08 18:18:02 +0200

    repo: initialize `configsever` Spring Config project

---
+++ configserver/src/test/kotlin/com/optimagrowth/configserver/ConfigserverApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.optimagrowth.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
