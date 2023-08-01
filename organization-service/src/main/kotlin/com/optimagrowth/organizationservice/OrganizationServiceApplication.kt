12736c011f5a381de0fabe67b5eacdf42b9f9c5b
Andrei Marshalov <andrei.marshalov@emlid.com>
2023-08-01 10:06:33 +0200

    repo: initialize the new `organization-service` project

---
+++ organization-service/src/main/kotlin/com/optimagrowth/organizationservice/OrganizationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.optimagrowth.organizationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrganizationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrganizationServiceApplication>(*args)
+}
