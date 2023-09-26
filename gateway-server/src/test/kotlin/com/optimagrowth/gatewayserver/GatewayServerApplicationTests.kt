2fa4f48a8963cffba7dc43553e53b8c93beb6352
Andrei Marshalov <andrei.marshalov@emlid.com>
2023-09-26 21:57:30 +0200

    add gateway-server project

---
+++ gateway-server/src/test/kotlin/com/optimagrowth/gatewayserver/GatewayServerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.optimagrowth.gatewayserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
